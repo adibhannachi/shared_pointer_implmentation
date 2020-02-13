eb15a50663f219e321105df9df4371fe16cab324
adib hannachi <adib.hannachi.tn@gmail.com>
2020-02-14 00:18:00 +0100

    Update the test scenario using GIVEN -> WHEN -> THEN

--- test/test_shared_ptr.cpp
+++ test/test_shared_ptr.cpp
@@ -1,33 +1,72 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "shared_ptr.h"
+#include "myclass.h"
 
-TEST_CASE( "SHARED POINTER TEST ", "[SHARED POINTER]" ) 
-{
-  easymile::shared_ptr<int> ptr1(new int(151));
-  REQUIRE( ptr1.use_count() == 1 );
-  {
-    easymile::shared_ptr<int> ptr2 = ptr1;
-    REQUIRE(ptr2.get() == ptr1.get() );
-    REQUIRE( ptr1.use_count() == 2 );
-  }
-  REQUIRE( ptr1.use_count() == 1 );
-  ptr1.~shared_ptr();
-  REQUIRE( ptr1.use_count() == 0 );
-  easymile::shared_ptr<int> sp(new int (5)); 
-  sp.reset (new int(2)); 
-  REQUIRE( sp.use_count() == 1 );
-  *(sp)=10;
-  REQUIRE( *sp == 10 );
-  sp.reset (new int);
-  *sp=20;
-  REQUIRE( sp.use_count() == 1 );
-  REQUIRE( *sp == 20 );
-  easymile::shared_ptr<int> ptr4(new int(151)); 
-  easymile::shared_ptr<int> ptr3(ptr4);
-  REQUIRE(ptr3.get() == ptr4.get() );
-  REQUIRE( ptr3.use_count() == 2 );
-  easymile::shared_ptr<int>&& ptr5(std::move(ptr3));
-  REQUIRE( ptr5.use_count() == 2 );
-  REQUIRE(ptr3.get() == ptr5.get() );
+
+SCENARIO( "SHARED POINTER TEST ", "[SHARED POINTER]" ) {
+
+    GIVEN( "A SHARED POINTER" ) {
+		easymile::shared_ptr<MyClass> ptr1(new MyClass(151));
+        REQUIRE( ptr1.use_count() == 1 );
+
+        WHEN( "MAKE ANOTHER SHARED POINTER" ) {
+			{
+				easymile::shared_ptr<MyClass> ptr2 = ptr1;
+				THEN( "COUNTER INCREESE" ) {
+					REQUIRE(ptr2.get() == ptr1.get() );
+					REQUIRE( ptr1.use_count() == 2 );
+				}
+				THEN( "BOTH POINTER POINT TO THE SOME INSTANCE" ) {
+					REQUIRE(ptr2.get() == ptr1.get());
+				}
+			}
+			WHEN( "IMPLICIT CALL TO  ~shared pointer" ) {
+				THEN( "COUNTER DECREESE" ) {
+					REQUIRE( ptr1.use_count() == 1 );
+				}
+			}
+        }
+		WHEN( "Make expilicit call to ~shared pointer" ) {
+			ptr1.~shared_ptr();
+			THEN( "COUNTER INCREESE" ) {
+				REQUIRE( ptr1.use_count() == 0 );
+			}
+		}
+	}
+	
+	GIVEN( "A SHARED POINTER To AN INSTANCE WITH DATA 5" ) {
+		easymile::shared_ptr<MyClass> ptr1(new MyClass(5)); 
+		REQUIRE( ptr1.use_count() == 1 );
+		THEN( "VALUE ACCEDED BY OPERATOR -> IS 5" ) {
+			REQUIRE( ptr1->getData() == 5 );
+			
+		}
+		WHEN( "MAKE RESET BY VALUE 2" ) {
+			ptr1.reset (new MyClass(2)); 
+			THEN( "VALUE IS 2" ) {
+				REQUIRE( ptr1->getData() == 2 );
+			}
+			THEN( "COUNTER IS 1" ) {
+				REQUIRE( ptr1.use_count() == 1 );
+			}
+		}
+	}
+	
+	GIVEN( "A SHARED POINTER FOR A SHARED POINTER" ) {
+		easymile::shared_ptr<MyClass> ptr3(new MyClass(151));
+		REQUIRE( ptr3.use_count() == 1 );
+
+		easymile::shared_ptr<MyClass> ptr4(ptr3);
+		REQUIRE( ptr3.use_count() == 2 );
+		WHEN( "A SHARED POINTER FOR A RVALUE REFERENCE" ) {
+			easymile::shared_ptr<MyClass>&& ptr5(std::move(ptr3));
+			THEN( "COUNTER WILL STILL 2" ) {
+				REQUIRE( ptr5.use_count() == 2 );
+			}
+			THEN( "BOTH POINTER POINT TO THE SOME DATA" ) {
+				REQUIRE(ptr3.get() == ptr5.get() );
+			}
+		}
+	}
 }
