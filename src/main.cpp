09a96d88d0c25aaa3f2298757730e9e682791c8a
Adib Hannachi <adib.hannachi.tn@gmail.com>
2020-02-14 11:00:16 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main() 
 { 
-    easymile::shared_ptr<MyClass> ptr1(new MyClass(1122)); 
-    cout << "VALUE : " <<  ptr1->getData() << endl;
+	easymile::shared_ptr<MyClass> ptr1(new MyClass(1122)); 
+	cout << "VALUE : " <<  ptr1->getData() << endl;
 	cout << "COUNT :: " << ptr1.use_count() << endl;
-    { 
-        easymile::shared_ptr<MyClass> ptr2 = ptr1; 
-        cout << "VALUE : " <<  ptr1->getData() << endl;
+	{ 
+		easymile::shared_ptr<MyClass> ptr2 = ptr1; 
+		cout << "VALUE : " <<  ptr1->getData() << endl;
 		cout << "COUNT :: " << ptr1.use_count() << endl;
-        ptr2->setData(5);
-        cout << "VALUE : " <<  ptr1->getData() << endl;
-    } 
+		ptr2->setData(5);
+		cout << "VALUE : " <<  ptr1->getData() << endl;
+	} 
 	cout << "COUNT :: " << ptr1.use_count() << endl;
 	cout << "VALUE : " <<  ptr1->getData() << endl;
-  
-    return 0; 
+
+	return 0; 
 } 
